4ebab1e77801faa80703d5ac89031d6339493cd4
statguy42 <69791564+statguy42@users.noreply.github.com>
2020-11-25 20:35:16 +0600

    added context manager for ThreadPoolExecutor in main()

--- weather_tk.py
+++ weather_tk.py
@@ -2,9 +2,9 @@ import concurrent.futures
 from gui import MainWindow
 
 def main():
-    threadworker = concurrent.futures.ThreadPoolExecutor(max_workers=1)
-    app = MainWindow(threadworker)
-    app.mainloop()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as threadworker:
+        app = MainWindow(threadworker)
+        app.mainloop()
 
 if __name__ == "__main__":
     main()
